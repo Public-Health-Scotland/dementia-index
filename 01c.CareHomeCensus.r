335a265cd9307007b5ee7b4a03043962555dd369
ekm87 <emily.moore@phs.scot>
2025-02-19 12:00:25 +0000

    clean carehome dates

--- 01c.CareHomeCensus.r
+++ 01c.CareHomeCensus.r
@@ -6,14 +6,39 @@
 CareHomeCensus <- read_excel("/PHI_conf/Dementia_Index/data/extracts/dementia_CareHomeCensus_2017_18_to_2023_24.xlsx")
 View(CareHomeCensus)
 
+table(year(CareHomeCensus$DateOfAdmission))
+table(year(CareHomeCensus$DateOfDischarge))
+##SOme dates are dicharge before admission - diacard? thhere are only a few...
+table(year(CareHomeCensus$DateOfDischarge) - year(CareHomeCensus$DateOfAdmission))
+##also some very long stays - so date of "diagnosis" is pretty vague
 
+##I tihnk some of the earlier "admission" dates are acutall DOBs in wrong column - they duplicate the DOB column
+
+table(year(CareHomeCensus$DateOfAdmission)[CareHomeCensus$DateOfAdmission == CareHomeCensus$DateOfBirth])
+
+
+
+hist(year(CareHomeCensus$DateOfAdmission))
 table(CareHomeCensus$DementiaMD)
 table(CareHomeCensus$DementiaNMD)
 table(CareHomeCensus$EthnicOrigin)
 table(CareHomeCensus$Sex)
 table(is.na(CareHomeCensus$UPI_NUMBER))
 table(CareHomeCensus$DementiaNMD, CareHomeCensus$DementiaMD, useNA="always")
+
 #Clean up variables with inconsistent values
+CareHomeCensus <-CareHomeCensus %>%
+  filter(!is.na(UPI_NUMBER)) %>% 
+  #remove records with dates that are not reliable
+  #remove if dicharge before admisison
+  filter(DateOfAdmission <= DateOfDischarge |is.na(DateOfDischarge)) %>% 
+  #remve if DOB is duplicated in admission field
+  filter(DateOfAdmission!=DateOfBirth | is.na(DateOfBirth)) %>%
+  filter(DateOfDischarge >= as.Date("2014-01-01") | is.na(DateOfDischarge))
+
+CareHomeCensus <-CareHomeCensus %>%
+  mutate(los_years = round((as.Date(DateOfDischarge)- as.Date(DateOfAdmission))/365.25,1))
+
 
 CareHomeCensus <-CareHomeCensus %>%
   filter(!is.na(UPI_NUMBER)) %>%
