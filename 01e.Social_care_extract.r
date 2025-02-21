4c19b4c04ac9ac6d68cc0f1d4646d2f068fc2a62
ekm87 <emily.moore@phs.scot>
2025-02-21 14:35:44 +0000

    split SC into carehome and non carehome records. date and postcode cleaning

--- 01e.Social_care_extract.r
+++ 01e.Social_care_extract.r
@@ -2,7 +2,7 @@
 ###01e. Social care extract
 ## run the setup file first#
 source("00.setup.r")
-
+library(lubridate)
 SC <- read_parquet(paste0(folder_data_path, "/extracts/Social_care_output.parquet"))
 
 ###infill dates where missing ####
@@ -10,6 +10,8 @@ SC <- read_parquet(paste0(folder_data_path, "/extracts/Social_care_output.parque
 ##work out "quarter client" start date. 
 ##flag dates that are just start of 1/4 or of year
 SC <- SC %>% 
+  ##remove spurious dates
+  mutate(start_date_of_care = case_when(year(start_date_of_care) < 1994 ~NA, T~ start_date_of_care)) %>%
   mutate(year_client_date = case_when(quarter_client %in% 1:3 ~ substr(financial_year,1,4), 
                                       quarter_client ==4 ~ paste0("20", substr(financial_year,6,7)))) %>%
   mutate(year_client_date = case_when(is.na(quarter_client) & financial_quarter_care_first %in% 1:3 ~ substr(financial_year,1,4), 
@@ -52,11 +54,21 @@ table(check_chi$n)
 ##POstcodes
 # clean by removing NK postcodes and NF postcodes (placeholders)
 # link simd to ID non-scottish postcodes in submitted or chi and replace with the other if the other is scottish 
-
-table(SC$chi %in% PDS$chi_number, useNA="always")  
-table(lubridate::year(SC$start_date_of_care), SC$chi %in% PDS$chi_number, useNA="always")  
+SC<- SC %>%
+  ##remove nk and nf
+  mutate(submitted_postcode = case_when(substr(submitted_postcode,1,2)=="NK" ~NA,
+                                        substr(submitted_postcode,1,2)=="NF" ~NA,
+                                        T~ submitted_postcode)) %>% 
+  mutate(ch_postcode = case_when(substr(ch_postcode,1,2)=="NK" ~NA,
+                                        substr(ch_postcode,1,2)=="NF" ~NA,
+                                        T~ ch_postcode))  %>%
+  ##definitive postcode is the CH postcode if present, them submitted , then the chi postcode
+  mutate(best_postcode= coalesce(ch_postcode, submitted_postcode, chi_postcode))
+  
+#table(SC$chi %in% PDS$chi_number, useNA="always")  
+#table(lubridate::year(SC$start_date_of_care), SC$chi %in% PDS$chi_number, useNA="always")  
 table(phsmethods::chi_check(SC$chi))
-table(phsmethods::chi_check(PDS$chi_number)) #some invalid CHI in PDS
+
 
 
 ##chi - UPI lookup####
@@ -75,24 +87,25 @@ table(SC$chi[is.na(SC$UPI_NUMBER)])
 
 SC <-SC %>% mutate(grouping_chi = case_when(!is.na(UPI_NUMBER) ~ UPI_NUMBER, T~chi))
 
-SC_first <-SC %>% group_by(grouping_chi) %>%
+### retain first social care care home record ####
+#and first social care any other type###
+SC_first <-SC %>% 
+  mutate(type_care2 = case_when(type_of_care=="care home" ~ "care home", 
+                                type_of_care=="not recorded" ~ "not recorded",
+                                T~ "other care")) %>%
+  group_by(grouping_chi, type_care2) %>%
   arrange(diagnosis_date) %>%
   slice(1) %>% 
-  select(chi, UPI_NUMBER, diagnosis_date, chi_date_of_birth, chi_date_of_birth, chi_postcode,
-        submitted_postcode, submitted_ethnic_group, ch_postcode, ch_admission_date, date_type) %>%
-  mutate(dementia_type = "99 Social care flag")
+  select(chi, UPI_NUMBER, diagnosis_date, chi_date_of_birth, chi_date_of_birth, best_postcode,
+         submitted_ethnic_group, ch_admission_date, date_type) %>%
+  mutate(dementia_type = "99 Social care flag") %>%
+  rename(type_of_care_group = type_care2) %>% 
+  rename(upi_number = grouping_chi) %>%
+  select(-c(chi, UPI_NUMBER))
 
 
-
-table(SC$financial_quarter_care_first,SC$quarter_client, useNA = "always")
-table(is.na(SC$start_date_of_care))
-table(is.na(SC$start_date_of_care), SC$quarter_client, useNA = "always")
-##Find best date - most have ab actual date. some who have none do have "quarter client"
-table(lubridate::year(SC$start_date_of_care))
-##some dates are placeholders (1900) or just wrong (1936??) remove dates before 1994
-###dates back to the 90s are plausible for long term care
-SC_first <- SC_first %>%
-  rename_with(.cols = everything(), function(x){paste0("SC_", x)})
+#SC_first <- SC_first %>%
+#  rename_with(.cols = everything(), function(x){paste0("SC_", x)})
 saveRDS(SC_first, "/PHI_conf/Dementia_Index/data/extracts/SC_clean.rds")
 
 
